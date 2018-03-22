5f6ed42959e6fa013c1ec045ad50298dfc9f2650
Rodrigo <rodrigolfsi@gmail.com>
2018-03-21 22:06:47 -0300

    Add aula_8

---
+++ aula_8/js/index.js
@@ -0,0 +1,58 @@
+var nome = 'Rodrigo';
+nome = "Rodrigo";
+var isSaturday = false;
+var year = 2018;
+
+var aluno = {
+    nome: 'Rodrigo',
+    email: 'rodrigo@gmail.com'
+};
+
+var school = null;
+
+console.log(aluno);
+console.log(school);
+
+school = 'Universidade do Vale do Sapucaí';
+console.log(school);
+
+var course;
+console.log(course);
+
+//comentario de linha 
+
+/*
+comentario 
+de 
+bloco
+*/
+
+function sum() {
+    
+    var a = 10;
+    var b = 15;
+    var c = a + b;
+    console.log(c);
+}
+
+sum();
+
+function sub(a, b) {
+    var c = a - b;
+    console.log(c);
+}
+
+sub(33, 13);
+
+function mul(a, b) {
+    var c = a * b;
+    return c;
+}
+
+var result = mul(10, 15);
+console.log(result);
+
+
+
+
+
