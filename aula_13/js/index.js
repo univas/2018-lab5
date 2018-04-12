ee7b2135d319013e99bd425fe3b27daf0cb03d9e
Rodrigo <rodrigolfsi@gmail.com>
2018-04-12 20:16:49 -0300

    Change code

--- aula_13/js/index.js
+++ aula_13/js/index.js
@@ -3,15 +3,53 @@ start();
 function start() {
     var btn = document.getElementById('addBtn');
     btn.onclick = function() {
-        addItemList();
+        var inputName = document.getElementById('name');
+        if (validateName(inputName)) {
+            addItemList(inputName);
+        } else {
+            alert('Por favor, digite o nome!');
+        }
+        clearInput(inputName);
     };
 }
 
-function addItemList() {
-    var inputName = document.getElementById('name');
-    var newListItem = document.createElement('li');
-    newListItem.innerHTML = inputName.value;
+function validateName(inputName) {
+    return inputName.value.trim() != '';
+}
+
+function addItemList(inputName) {
+    var newListItem = createItemList(inputName.value);
 
     var list = document.getElementById('contactList');
     list.appendChild(newListItem);
 }
+
+function createItemList(content) {
+    var newItemList = document.createElement('li');
+    newItemList.innerHTML = content;
+    return newItemList;
+}
+
+function clearInput(inputName) {
+    inputName.value = '';
+    inputName.focus();
+}
+
+
+/*
+12-04
+Diego Fraga
+2 Bruno
+Rafael
+Franz
+Rangel
+Rafaela
+Sulivan
+Ewerton
+Samuel
+Jonathan
+Diego Nogueira
+Diego Alves
+Ingrid
+
+*/
