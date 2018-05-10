ce6dbec2e12aa4bd255b2dc69d9c05323ebd0a64
Rodrigo <rodrigolfsi@gmail.com>
2018-05-09 21:55:10 -0300

    Add aula_19

---
+++ aula_19/js/index.js
@@ -0,0 +1,53 @@
+var factor = 5;
+start();
+
+function start() {
+    var btn = document.getElementById('btnStart');
+    btn.onclick = function() {
+        setInterval(function() {
+            moveImage();
+        }, 50);
+        btn.disabled = true;
+
+        setTimeout(timeoutTest, 2000);
+    };
+}
+
+function timeoutTest() {
+    console.log(new Date());
+}
+
+function moveImage() {
+    var image = document.getElementById('imgBrazil');
+    var currentMarging = image.style.marginLeft.replace("px", "");
+    
+    if (currentMarging == "") {
+        currentMarging = 0;
+    } else {
+        currentMarging = parseInt(currentMarging, 10);
+    }
+
+    if (currentMarging == 1000) {
+        factor = -5;
+    } else if (currentMarging == 0) {
+        factor = 5;
+    }
+
+    currentMarging += factor;
+    image.style.marginLeft = currentMarging + 'px';
+
+}
+
+/*
+Jonathan
+3 Diego (Nogueira não veio)
+Sulivan
+Everton
+2 Bruno
+Ingrid
+Franz
+Daniel
+Rangel
+Samuel
+Julio
+*/
